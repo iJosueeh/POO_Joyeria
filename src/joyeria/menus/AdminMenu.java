251866ba9a0c0ad872cecca0428b26dd8abc33ca
HOME <HOME@DESKTOP-1MP425B>
2024-10-10 22:33:21 -0500

    Clase de menú administrativo agregado

---
+++ src/joyeria/menus/AdminMenu.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package joyeria.menus;
+
+import java.util.Scanner;
+
+public class AdminMenu {
+
+    public void mostrarMenu(Scanner scanner) {
+        boolean continuarMenu = true;
+        
+        do {
+            System.out.println("\n>>=================================================<<");
+            System.out.println("    Menú Administrativo - Joyas del Corazón");
+            System.out.println(">>=================================================<<\n");
+            System.out.println("→ [1] Gestión de Usuarios");
+            System.out.println("→ [2] Gestión de Productos");
+            System.out.println("→ [3] Gestión de Pedidos");
+            System.out.println("→ [4] Gestión de Cupones");
+            System.out.println("→ [5] Gestión de Reseñas");
+            System.out.println("→ [6] Ver Reportes");
+            System.out.println("→ [7] Volver al Menú Principal");
+            System.out.println("→ [8] Cerrar Programa");
+            System.out.print("Seleccione su opción: ");
+            int opcionMenu = scanner.nextInt();
+            
+            switch (opcionMenu) {
+                case 1:
+                    System.out.println("Gestion de Usuarios");
+                    break;
+                case 2:
+                    System.out.println("Gestion de Productos");
+                    break;
+                case 3:
+                    System.out.println("Gestion de Pedidos");
+                    break;
+                case 4:
+                    System.out.println("Gestion de Cupones");
+                    break;
+                case 5:
+                    System.out.println("Gestion de Reseñas");
+                    break;
+                case 6:
+                    System.out.println("Ver reportes");
+                    break;
+                case 7:
+                    // Volviendo al menú principal
+                    System.out.println("Volviendo al menú principal..\n");
+                    continuarMenu = false;
+                    break;
+                case 8:
+                    // Cerrar el programa
+                    System.out.println("Cerrando el programa, vuelva pronto!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no valida, ingresa el número indicado en las opciones.\n");
+            }
+            
+        } while (continuarMenu);
+    }
+}
