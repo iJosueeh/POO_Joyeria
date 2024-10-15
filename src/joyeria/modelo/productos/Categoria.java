68a0ca36917727b451d320b3093883c4937d95fc
hp <hp@LAPTOP-V6H9P5AD>
2024-10-15 18:51:42 -0500

    Se implemento la clase Producto

--- src/joyeria/modelo/productos/Categoria.java
+++ src/joyeria/modelo/productos/Categoria.java
@@ -1,9 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package joyeria.modelo.productos;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Categoria {
+   private int idCategoria;
+   private String nombre;
+   private Set <Producto> productos;
+
+    public Categoria(int idCategoria, String nombre, Set<Producto> productos) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+        this.productos = new HashSet<>();//almacenar sin duplicados
+        
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Set<Producto> productos) {
+        this.productos = productos;
+    }
     
+    
+    public void Agregar_P(Producto producto){
+        productos.add(producto);
+        System.out.println("Producto agregado a la categoria "+ nombre+ ":"+ producto.getNombre());
+        
+    }
+    public void Eliminar_P(Producto producto) {
+        if (productos.contains(producto)) {//verifica si existe
+            productos.remove(producto);
+            System.out.println("Producto eliminado de la categoria " + nombre + ": " + producto.getNombre());
+        } else {
+            System.out.println("El producto no se encuentra en la categoria.");
+        }
+    }
+   //metodo para obtener un conjunto de productos en la categoria
+     public Set<Producto> obtenerProductos() {
+        return productos;
+     }
+     public void imprimir(){
+         System.out.println("Productos de la categoria"+ nombre+":");
+         for(Producto producto: productos){
+             System.out.println(producto.getNombre());
+         }
+     }
+     
 }
--- src/joyeria/modelo/productos/Proveedor.java
+++ src/joyeria/modelo/productos/Proveedor.java
@@ -1,10 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package joyeria.modelo.productos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class Proveedor {
+    private int idProveedor;
+    private String nombre;
+    private String telefono;
+    private String direccion;
+    private static List<Proveedor> listaProveedores = new ArrayList<>();// base de datos provedores
+
+    public Proveedor(int idProveedor, String nombre, String telefono, String direccion) {
+        this.idProveedor = idProveedor;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+    //metodo registro proveedores
+    public void registrar_Provedor(){
+        listaProveedores.add(this);
+        System.out.println("Se registro al proveedor exitosamente"+ this);
+    }
+    //metodo actualizar datos de proveedores
+    public void Actualizar_Datos(){
+        Scanner scanner = new Scanner (System.in);
+         System.out.println("Actualización de datos del proveedor con ID: " + idProveedor);
+         
+         System.out.println("Nuevo nombre :  ");
+         this.nombre = scanner.nextLine();
+         
+         System.out.println("Nuevo telefono: ");
+         this.telefono = scanner.nextLine();
+         
+         System.out.println("Nueva direccion: ");
+         this.direccion = scanner.nextLine();
+         System.out.println("Datos del proveedor actualizados exitosamente:");
+         System.out.println(this);
+    }
+    public static void Mostrar_Proveedores(){
+        if(listaProveedores.isEmpty()){
+            System.out.println("No hay proveedores registrados: ");
+            
+        }else{
+            System.out.println("Lista de proveedores registrados: ");
+            for(Proveedor proveedor:listaProveedores){
+                System.out.println(proveedor);
+            }
+           
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" + "idProveedor=" + idProveedor + ", nombre=" + nombre + ", telefono=" + telefono + ", direccion=" + direccion + '}';
+    }
+     public static Proveedor buscarProveedorPorId(int idProveedor) {
+        for (Proveedor proveedor : listaProveedores) {
+            if (proveedor.idProveedor == idProveedor) {
+                return proveedor;
+            }
+        }
+        return null;
+    }
+
+   
+    
     
 }
