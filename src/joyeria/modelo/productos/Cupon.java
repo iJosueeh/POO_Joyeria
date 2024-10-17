693af7d851ec2195a5c4b53654c58fabe141cf16
hp <hp@LAPTOP-V6H9P5AD>
2024-10-17 11:36:01 -0500

    Se implemento la clase Producto

--- src/joyeria/modelo/productos/Cupon.java
+++ src/joyeria/modelo/productos/Cupon.java
@@ -1,6 +1,78 @@
 
 package joyeria.modelo.productos;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cupon {
+    private int idCupon;
+    private String codigo;
+    private String valorDescuento;
+    private LocalDate fechaExpiracion;
+    private Map<String, Cupon> listaCupones;
+
+    public Cupon(int idCupon, String codigo, String valorDescuento, LocalDate fechaExpiracion, Map<String, Cupon> listaCupones) {
+        this.idCupon = idCupon;
+        this.codigo = codigo;
+        this.valorDescuento = valorDescuento;
+        this.fechaExpiracion = fechaExpiracion;
+        this.listaCupones = new HashMap<>();
+        
+    }
+    public boolean validarcupon(){
+        return !LocalDate.now().isAfter(fechaExpiracion);
+        
+    }
+    public double aplicarcupon(double total){
+        if(validarcupon()){
+            double descuento= Double.parseDouble(valorDescuento.replace("%", ""))/100;
+            return total-(total*descuento);
+        }else{
+            System.out.println("El cupon ha expirado");
+            return total;
+        }
+    }
+
+    public int getIdCupon() {
+        return idCupon;
+    }
+
+    public void setIdCupon(int idCupon) {
+        this.idCupon = idCupon;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getValorDescuento() {
+        return valorDescuento;
+    }
+
+    public void setValorDescuento(String valorDescuento) {
+        this.valorDescuento = valorDescuento;
+    }
+
+    public LocalDate getFechaExpiracion() {
+        return fechaExpiracion;
+    }
+
+    public void setFechaExpiracion(LocalDate fechaExpiracion) {
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    public Map<String, Cupon> getListaCupones() {
+        return listaCupones;
+    }
+
+    public void setListaCupones(Map<String, Cupon> listaCupones) {
+        this.listaCupones = listaCupones;
+    }
+    
     
 }
--- src/joyeria/modelo/productos/Reseña.java
+++ src/joyeria/modelo/productos/Reseña.java
@@ -1,9 +1,80 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package joyeria.modelo.productos;
 
+import joyeria.modelo.usuarios.Cliente;
+
 public class Reseña {
+    private int idReseña;
+    private Cliente cliente;
+    private Producto producto;
+    private String comentario;
+    private int puntuacion;
+
+    public Reseña(int idReseña, Cliente cliente, Producto producto, String comentario, int puntuacion) {
+        this.idReseña = idReseña;
+        this.cliente = cliente;
+        this.producto = producto;
+        this.comentario = comentario;
+        this.puntuacion = puntuacion;
+    }
+    
+    public void CrearReseña(){
+        System.out.println("Reseña creada: "+this.comentario + "Con una puntacion: "+ this.puntuacion);
+    }
+    public void EditarR(String nuevoComentario, int NuevoPuntaje){
+        this.comentario= nuevoComentario;
+        this.puntuacion= NuevoPuntaje;
+        System.out.println("Reseña editada:"+ this.comentario + "Con la nueva puntacion" + this.puntuacion);
+    }
+    public void EliminarR(){
+        System.out.println("Reseña con ID: "+ this.idReseña + "ha sido eliminada");
+        
+    }
+    public void mostrarR(){
+        System.out.println("Reseña del Producto:"+ this.producto.getNombre());
+        System.out.println("Comentario: "+ this.comentario);
+        System.out.println("Puntuacion: " + this.puntuacion);
+        
+    }
+
+    public int getIdReseña() {
+        return idReseña;
+    }
+
+    public void setIdReseña(int idReseña) {
+        this.idReseña = idReseña;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
     
 }
