4aeab96e93b6351261815b0080fd7dbc1e569ab7
HOME <HOME@DESKTOP-1MP425B>
2024-10-17 23:37:31 -0500

    Implementando atributos y metodos a Pago

--- src/joyeria/modelo/pedidos/Pago.java
+++ src/joyeria/modelo/pedidos/Pago.java
@@ -4,6 +4,72 @@
  */
 package joyeria.modelo.pedidos;
 
+import java.time.LocalDate;
+
 public class Pago {
-    
+
+    private int idPago;
+    private double monto;
+    private LocalDate fechaPago;
+    private String metodoPago;
+
+    public Pago(int idPago, double monto, LocalDate fechaPago, String metodoPago) {
+        this.idPago = idPago;
+        this.monto = monto;
+        this.fechaPago = fechaPago;
+        this.metodoPago = metodoPago;
+    }
+
+    public int getIdPago() {
+        return idPago;
+    }
+
+    public void setIdPago(int idPago) {
+        this.idPago = idPago;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public LocalDate getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDate fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public Boolean ProcesarPago() {
+        if (validarMetodoPago()) {
+            System.out.println("El pago de " + monto + " fue procesado exitosamente con " + metodoPago);
+            return true;
+        } else {
+            System.out.println("Error: método de pago no válido.");
+            return false;
+        }
+    }
+
+    public Boolean validarMetodoPago() {
+        String[] metodosAceptados = {"Tarjeta", "Efectivo", "Transferencia"};
+        for (String metodo : metodosAceptados) {
+            if (metodo.equalsIgnoreCase(metodoPago)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
